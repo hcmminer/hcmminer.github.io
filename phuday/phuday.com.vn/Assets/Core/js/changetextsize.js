0cc1632a2635fc718109293c0bc34f47e304e563
bandang <hcmminer@gmai.com>
2022-05-20 14:26:44 +0700

    project4

---
+++ phuday/phuday.com.vn/Assets/Core/js/changetextsize.js
@@ -0,0 +1,20 @@
+﻿function ChangeTextSize(value) {
+    $(".detail").children().each(function () {
+        var size = parseInt($(this).css("font-size"));
+        var lineheight = parseInt($(this).css("line-height"));
+        size = size + value + "px";
+        lineheight = lineheight + value + "px";
+        $(this).css({
+            'font-size': size,
+            'lineheight':lineheight
+        });
+    });
+    var sized = parseInt($(".detail").css("font-size"));
+    var lineheightd = parseInt($(".detail").css("line-height"));
+    sized = sized + value + "px";
+    lineheightd = lineheightd + value + "px";
+    $(".detail").css({
+        'font-size': sized,
+        'lineheight': lineheightd
+    });
+}
